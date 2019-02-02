f9dfac4fc5b3d034bdae36d528711cb308b0cd9d
Johan Lindgren <johan@lindgr3n.com>
2019-02-02 22:24:05 +0100

    Interaction
    Implemented interaction with objects

--- src/index.js
+++ src/index.js
@@ -25,10 +25,87 @@ const objects = [];
 const players = [];
 
 // Add objects
-objects.push(Object.assign({ x: 10, y: 10 }, tileObjects.exit));
-objects.push(Object.assign({ x: 8, y: 7 }, tileObjects.tree));
-objects.push(Object.assign({ x: 8, y: 8 }, tileObjects.tree));
-objects.push(Object.assign({ x: 8, y: 9 }, tileObjects.tree));
+objects.push(
+  Object.assign(
+    {
+      x: 10,
+      y: 10,
+      interact: function(interactor) {
+        console.log('Wohoo you found the exit!');
+      }
+    },
+    tileObjects.exit
+  )
+);
+objects.push(
+  Object.assign(
+    {
+      x: 8,
+      y: 7,
+      health: 5,
+      loot: { type: 'wood', amount: 3 },
+      interact: function(interactor) {
+        if (interactor.hasOwnProperty('damage')) {
+          this.health = this.health - interactor.damage;
+        }
+        console.log(`Choop! My current health ${this.health}`);
+        // Dead return the loot!
+        if (this.health <= 0) {
+          removeDead(this);
+        }
+
+        return this.loot;
+      }
+    },
+    tileObjects.tree
+  )
+);
+objects.push(
+  Object.assign(
+    {
+      x: 8,
+      y: 8,
+      health: 5,
+      loot: { type: 'wood', amount: 3 },
+      interact: function(interactor) {
+        if (interactor.hasOwnProperty('damage')) {
+          this.health = this.health - interactor.damage;
+        }
+        console.log(`Choop! My current health ${this.health}`);
+        // Dead return the loot!
+        if (this.health <= 0) {
+          removeDead(this);
+        }
+
+        return this.loot;
+      }
+    },
+    tileObjects.tree
+  )
+);
+objects.push(
+  Object.assign(
+    {
+      x: 8,
+      y: 9,
+      health: 5,
+      loot: { type: 'wood', amount: 1 },
+      interact: function(interactor) {
+        if (interactor.hasOwnProperty('damage')) {
+          this.health = this.health - interactor.damage;
+        }
+        console.log(`Choop! My current health ${this.health}`);
+        // Dead return the loot!
+        if (this.health <= 0) {
+          removeDead(this);
+        }
+
+        return this.loot;
+      }
+    },
+    tileObjects.tree
+  )
+);
 
 // Add players
 // 0 index is current player
@@ -47,6 +124,17 @@ generateMap();
 // Render map
 update();
 
+function removeDead(object) {
+  // Get index of the provided object
+  const foundIndex = objects.findIndex(
+    o => o.x === object.x && o.y === object.y
+  );
+
+  // Remove it from the objects list
+  if (foundIndex > -1) {
+    objects.splice(foundIndex, 1);
+  }
+}
 // Generate map
 function generateMap() {
   for (let j = 0; j < o.height; j++) {
@@ -88,14 +176,63 @@ function update() {
   drawPlayers();
 }
 
-function canMoveTo(object, dir) {
-  const tileAt = map[object.y + dir.y][object.x + dir.x];
-  console.log(tileAt);
-  if (tileAt.types.find(type => type === 'block')) {
+function getTileAt(pos) {
+  const tileAt = map[pos.y][pos.x];
+  return tileAt;
+}
+
+function getObjectAt(pos) {
+  const object = objects.find(o => o.x === pos.x && o.y === pos.y);
+  return object;
+}
+
+function canMoveTo(player, pos) {
+  if (!player || !pos) {
+    return true;
+  }
+  const object = getObjectAt(pos);
+  // If there exist a object we know that interaction made it still exist
+  if (object) {
+    return false;
+  }
+
+  const tile = getTileAt(pos);
+  // No tile exist allow pass
+  if (!tile) {
+    return true;
+  }
+
+  // Type of non passable prevent moving
+  if (tile.types.find(type => type === 'block')) {
     return false;
   }
 
   return true;
+}
+
+function interact(player, pos) {
+  if (!player || !pos) {
+    return;
+  }
+  const object = getObjectAt(pos);
+  // No object exist carry on
+  if (!object) {
+    return;
+  }
+
+  if (object.types.find(type => type === 'exit')) {
+    object.interact(player);
+  }
+
+  if (object.types.find(type => type === 'breakable')) {
+    // Break it
+    const loot = object.interact(player);
+    if (loot) {
+      console.log('Wohoo we got loot: ', loot);
+    }
+  }
+
+  console.log('Time to interact with: ', object);
 }
 
 // Listeners
@@ -122,10 +259,18 @@ document.body.addEventListener('keydown', function(e) {
   if (code === KEYS.VK_DOWN) {
     dir.y = 1;
   }
-  const canMove = canMoveTo(players[0], dir);
 
+  const player = players[0];
+  const nextPos = {
+    x: player.x + dir.x,
+    y: player.y + dir.y
+  };
+
+  // interact
+  interact(player, nextPos);
+
+  const canMove = canMoveTo(player, nextPos);
   if (!canMove) {
-    // Check if we can interact
     return;
   }
 
